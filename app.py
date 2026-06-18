04077c5156402b90f2e7cdd41bbdefbf0ff945bd
Carlos Eloy Ornelas de la Torre <Eloy@Apples-MacBook-Pro.local>
2026-06-18 11:56:05 -0600

    Fix coin toss chart

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ st.header('Lanzar una moneda')
 chart = st.line_chart([0.5])
 
 def toss_coin(n):
-
     trial_outcomes = scipy.stats.bernoulli.rvs(p=0.5, size=n)
 
     mean = None
@@ -15,18 +14,25 @@ def toss_coin(n):
     outcome_1_count = 0
 
     for r in trial_outcomes:
-        outcome_no +=1
+        outcome_no += 1
+
         if r == 1:
             outcome_1_count += 1
+
         mean = outcome_1_count / outcome_no
         chart.add_rows([mean])
         time.sleep(0.05)
 
     return mean
 
-number_of_trials = st.slider('¿Número de intentos?', 1, 1000, 10)
+number_of_trials = st.slider(
+    '¿Número de intentos?', 1, 1000, 10
+)
+
 start_button = st.button('Ejecutar')
 
 if start_button:
-    st.write(f'Experimento con {number_of_trials} intentos en curso.')
+    st.write(
+        f'Experimento con {number_of_trials} intentos en curso.'
+    )
     mean = toss_coin(number_of_trials)

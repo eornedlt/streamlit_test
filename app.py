4134585bc7be31bc4dcc96e4236b04fb75eb373e
Carlos Eloy Ornelas de la Torre <Eloy@Apples-MacBook-Pro.local>
2026-06-18 12:30:56 -0600

    Fix chart for current Streamlit

--- app.py
+++ app.py
@@ -15,22 +15,19 @@ st.header('Lanzar una moneda')
 chart = st.line_chart([0.5])
 
 def toss_coin(n):
+
     trial_outcomes = scipy.stats.bernoulli.rvs(p=0.5, size=n)
 
     mean = None
     outcome_no = 0
     outcome_1_count = 0
-    results = [0.5]
 
     for r in trial_outcomes:
-        outcome_no += 1
-
+        outcome_no +=1
         if r == 1:
             outcome_1_count += 1
-
         mean = outcome_1_count / outcome_no
-        results.append(mean)
-        chart.line_chart(results)
+        chart.add_rows([mean])
         time.sleep(0.05)
 
     return mean
@@ -40,4 +37,17 @@ start_button = st.button('Ejecutar')
 
 if start_button:
     st.write(f'Experimento con {number_of_trials} intentos en curso.')
+    st.session_state['experiment_no'] += 1
     mean = toss_coin(number_of_trials)
+    st.session_state['df_experiment_results'] = pd.concat([
+        st.session_state['df_experiment_results'],
+        pd.DataFrame(data=[[st.session_state['experiment_no'],
+                            number_of_trials,
+                            mean]],
+                     columns=['no', 'iteraciones', 'media'])
+        ],
+        axis=0)
+    st.session_state['df_experiment_results'] = st.session_state['df_experiment_results'].reset_index(drop=True)
+
+st.write(st.session_state['df_experiment_results'])
+
